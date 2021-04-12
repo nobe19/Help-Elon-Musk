71dbfbe910f402c1f3503cc862a73c037b65618e
Nipuni Obe <obe@gmail.com>
2021-04-12 18:45:48 -0400

    rocket in class

--- Help Elon Musk/ViewController.swift
+++ Help Elon Musk/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var rocketImageView: UIImageView!
+    var imageNumber = 0
+    let totalImages = 5
+    var yAtLanding: CGFloat!
+    var audioPlayer: AVAudioPlayer!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        yAtLanding = rocketImageView.frame.origin.y
+    }
+    
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("😡 ERROR: \(error.localizedDescription) Couldn't initialize AVAudioPlayer object.")
+            }
+        } else {
+            print("😡 ERROR: Couldn't read data from file sound0")
+        }
     }
 
-
+    @IBAction func rocketTapped(_ sender: UITapGestureRecognizer) {
+        imageNumber += 1
+        if imageNumber >= totalImages {
+            imageNumber = 0
+        }
+        rocketImageView.image = UIImage(named: "rocket\(imageNumber)")
+    }
+    @IBAction func blastOffPressed(_ sender: UIButton) {
+        
+        if rocketImageView.frame.origin.y < yAtLanding {
+            playSound(name: "rocketsound")
+            sender.setTitle("Blast Off!", for: .normal)
+            
+            
+            UIView.animate(withDuration: 2.0, animations: {
+                self.rocketImageView.frame.origin.y = self.yAtLanding
+            }) { _ in
+                self.audioPlayer.stop()
+            }
+        } else {
+            playSound(name: "rocketsound")
+            sender.setTitle("Return", for: .normal)
+            UIView.animate(withDuration: 2.0, animations: {
+                self.rocketImageView.frame.origin.y = -self.rocketImageView.frame.height
+            }) { _ in
+                self.audioPlayer.stop()
+            }
+        }
+    }
 }
 
